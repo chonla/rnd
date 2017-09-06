e3f9df6a777fb51197011b2ee97e1bf69d2e1007
Chonlasith Jucksriporn <chonlasith@gmail.com>
2017-09-06 22:21:08 +0700

    Change Random func name to OneOf to reflect actual use

--- rand.go
+++ rand.go
@@ -5,8 +5,8 @@ import (
 	"time"
 )
 
-// Random return one of input
-func Random(l []interface{}) interface{} {
+// OneOf return one of input
+func OneOf(l []interface{}) interface{} {
 	r := rand.New(rand.NewSource(time.Now().UnixNano()))
 	return l[r.Intn(len(l))]
 }
